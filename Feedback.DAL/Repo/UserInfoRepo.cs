d3a777d53c6fdf765d84bcb92f083afd13fe2048
Santosh Mainali <cyx977@outlook.com>
2019-02-24 09:26:37 +0545

    init

---
+++ Feedback.DAL/Repo/UserInfoRepo.cs
@@ -0,0 +1,24 @@
+﻿using Feedback.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feedback.DAL.Repo
+{
+    public interface IUserInfoRepo
+    {
+        List<UserInfo> GetUsers();
+    }
+    public class UserInfoRepo : IUserInfoRepo
+    {
+        public List<UserInfo> GetUsers()
+        {
+            using(FeedBackEntities feedBackEntities = new FeedBackEntities())
+            {
+                return feedBackEntities.UserInfoes.ToList();
+            }
+        }
+    }
+}
---
+++ Feedback.Service/Service/UserInfoService.cs
@@ -0,0 +1,27 @@
+﻿using Feedback.DAL.Model;
+using Feedback.DAL.Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feedback.Service.Service
+{
+    public interface IUserInfoService
+    {
+        List<UserInfo> GetUsers();
+    }
+    public class UserInfoService : IUserInfoService
+    {
+        private IUserInfoRepo _userInfoRepo;
+        public UserInfoService(IUserInfoRepo userInfoRepo)
+        {
+            _userInfoRepo = userInfoRepo;
+        }
+        public List<UserInfo> GetUsers()
+        {
+            return _userInfoRepo.GetUsers();
+        }
+    }
+}
